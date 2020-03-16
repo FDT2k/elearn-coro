387ed7adb8b660779be286f968615663c08cb8f2
Emilie K <emilie@karsegard.ch>
2020-03-16 04:26:51 +0100

    change color for Elm

--- 00_elearning/src/components/Files/index.js
+++ 00_elearning/src/components/Files/index.js
@@ -5,18 +5,42 @@ import React from 'react'
 export default props => {
 
   return (
-    <div className="files">
-      
-      <div className="theme-dark">
-        <header className="main-header  flex just-center">
+    <div className="files theme-light_elm cover just-between align-stretch">
+
+      <div className="theme-dark_elm">
+        <header className="main-header">
           <h1>Classe n°1132</h1>
         </header>
       </div>
-      <section className="theme-light">
-        <div>
-          <button onClick={props.handleLogin}>Login</button>
+
+        <div className="contact-us">
+          <section className="flex just-center">
+              <form className="form_login flex flex-column">
+                <div className="floating-label-wrap">
+                  <input
+                    type="text"
+                    id="fieldMail"
+                    className="floating-label-field"
+                    name="email"
+                    placeholder="Quel est ton code ?"
+                  />
+                  <label htmlFor="fieldMail" className="floating-label">
+                    Quel est ton code ?
+                  </label>
+                </div>
+
+                <button onClick={props.handleLogin}>Login</button>
+              </form>
+          </section>
+
         </div>
-      </section>
+
+        <footer className="footer">
+          <div>
+            <p>2020 - Designed with love, cocoa and coffe by <a rel="noopener noreferrer" target="_blank" href="https://www.geekagency.ch">GeekAgency</a> - Switzerland</p>
+          </div>
+        </footer>
+
     </div>
 
   )
--- 00_elearning/src/components/Login/index.js
+++ 00_elearning/src/components/Login/index.js
@@ -5,15 +5,15 @@ import React from 'react'
 export default props => {
 
   return (
-    <div className="theme-light cover just-between align-stretch">
+    <div className="theme-light_elm cover just-between align-stretch">
 
-      <div className="theme-dark">
+      <div className="theme-dark_elm">
         <header className="main-header">
           <h1>Elearn-20</h1>
         </header>
       </div>
 
-        <div className="theme-light contact-us">
+        <div className="contact-us">
           <section className="flex just-center">
               <form className="form_login flex flex-column">
                 <div className="floating-label-wrap">
@@ -35,9 +35,9 @@ export default props => {
 
         </div>
 
-        <footer className="theme-light footer">
+        <footer className="footer">
           <div>
-            <p>2020 - Designed with love, cocoa and coffe by <a target="_blank" href="https://www.geekagency.ch">GeekAgency</a> - Switzerland</p>
+            <p>2020 - Designed with love, cocoa and coffe by <a rel="noopener noreferrer" target="_blank" href="https://www.geekagency.ch">GeekAgency</a> - Switzerland</p>
           </div>
         </footer>
 
