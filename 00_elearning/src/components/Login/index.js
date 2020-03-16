3ca435d63f6e07bf51715a85aef3fc68135e2f61
Emilie K <emilie@karsegard.ch>
2020-03-16 03:35:15 +0100

    login page done

--- 00_elearning/src/components/Login/index.js
+++ 00_elearning/src/components/Login/index.js
@@ -5,17 +5,42 @@ import React from 'react'
 export default props => {
 
   return (
-    <div className="login">
+    <div className="theme-light cover just-between align-stretch">
+
       <div className="theme-dark">
-        <header className="main-header  flex just-center">
+        <header className="main-header">
           <h1>Elearn-20</h1>
         </header>
       </div>
-      <section className="theme-light">
-        <div>
-          <button onClick={props.handleLogin}>Login</button>
+
+        <div className="theme-light contact-us">
+          <section className="flex just-center">
+              <form className="form_login flex flex-column">
+                <div className="floating-label-wrap">
+                  <input
+                    type="text"
+                    id="fieldMail"
+                    className="floating-label-field"
+                    name="email"
+                    placeholder="Quel est ton code ?"
+                  />
+                  <label htmlFor="fieldMail" className="floating-label">
+                    Quel est ton code ?
+                  </label>
+                </div>
+
+                <button onClick={props.handleLogin}>Me connecter</button>
+              </form>
+          </section>
+
         </div>
-      </section>
+
+        <footer className="theme-light footer">
+          <div>
+            <p>2020 - Designed with love, cocoa and coffe by GeekAgency - Switzerland</p>
+          </div>
+        </footer>
+
     </div>
   )
 
