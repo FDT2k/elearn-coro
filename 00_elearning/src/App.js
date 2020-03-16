cbea9b2a5b09b70c2516aa8adeda99f4587c7283
Emilie K <emilie@karsegard.ch>
2020-03-16 01:48:52 +0100

    mise en place sass + header

--- 00_elearning/src/App.js
+++ 00_elearning/src/App.js
@@ -1,7 +1,8 @@
 import React,{useState} from 'react';
-
 import Files from './components/Files'
 import Login from './components/Login'
+
+
 
 function App() {
   const [logged,setLogged] = useState(false)
--- 00_elearning/src/components/Files/index.js
+++ 00_elearning/src/components/Files/index.js
@@ -5,7 +5,18 @@ import React from 'react'
 export default props => {
 
   return (
-    <div>Programme</div>
+    <div className="files">
+      <div className="theme-dark">
+        <header className="main-header  flex just-center">
+          <h1>Classe n°1132</h1>
+        </header>
+      </div>
+      <section className="theme-light">
+        <div>
+          <button onClick={props.handleLogin}>Login</button>
+        </div>
+      </section>
+    </div>
 
   )
 
